4d5d2132d8c656e8a4ca0ceb5c1b5fb3f8b1b4d7
gtblackmore <gtblackmore@gmail.com>
2020-06-11 23:35:35 -0400

    add scopes to tenants and properties

--- app/models/property.rb
+++ app/models/property.rb
@@ -2,4 +2,12 @@ class Property < ApplicationRecord
   belongs_to :entity
   has_many :tenants, dependent: :destroy
   validates :address, :city, :state, :zip, :county, presence: true
+ 
+  def occupied?
+    !self.tenants.empty?
+  end
+  
+  def total_owed
+    self.tenants.delinquencies.sum("outstanding_balance")
+  end
 end
--- app/models/tenant.rb
+++ app/models/tenant.rb
@@ -2,4 +2,12 @@ class Tenant < ApplicationRecord
   belongs_to :property
   validates :first_name, :last_name, :email, :phone, presence: true
   validates :email, uniqueness: true
+
+  def self.delinquencies
+    where("outstanding_balance > 0")
+  end
+
+  def self.total_owed
+    sum("outstanding_balance")
+  end
 end
